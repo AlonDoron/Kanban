50f12d61427d843c26cbb6377dea1961423b1ce0
A10ND <alon121211@gmail.com>
2020-03-17 23:14:53 +0200

    Updated UserInfo to now work with flexbox styling

--- src/components/common/UserInfo.js
+++ src/components/common/UserInfo.js
@@ -1,12 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 import { Avatar } from "./";
+
+const UserInfoWrapper = styled.div({
+  display: "inline-flex",
+  flexDirection: "row",
+  border: "1px solid #0984e3"
+});
+
+const UsernameWrapper = styled.h3({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  color: "#0984e3"
+});
 
 function UserInfo(props) {
   return (
-    <div className="UserInfo">
+    <UserInfoWrapper>
+      <UsernameWrapper>{props.user.name}</UsernameWrapper>
       <Avatar user={props.user} />
-      <div className="Username">{props.user.name}</div>
-    </div>
+    </UserInfoWrapper>
   );
 }
 
