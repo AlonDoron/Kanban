dfbd76c00142edf6f58e02062d485f0caf9897d6
A10ND <alon121211@gmail.com>
2020-03-19 18:37:48 +0200

    Updated and styled TodoItem component and it's code

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -1,16 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 import { UserInfo } from "../common";
 
-function TodoItem(props) {
+const ItemWrapper = styled.div({
+  border: "1px solid #0984e3",
+  display: "inline-block",
+  maxWidth: "25%"
+});
+
+const TitleWrapper = styled.h3({
+  color: "#00cec9",
+  display: "flex",
+  flexDirection: "row",
+  marginBottom: "0px"
+});
+
+let TodoItem = props => {
   return (
-    <div className="Item">
-      <header className="ItemInfo">
-        <h2 className="Title">{props.title}</h2>
+    <ItemWrapper>
+      <header>
+        <TitleWrapper>{props.title}</TitleWrapper>
         <UserInfo user={props.user} />
       </header>
-      <section className="Content">{props.content}</section>
-    </div>
+      <section>{props.content}</section>
+    </ItemWrapper>
   );
-}
+};
 
 export default TodoItem;
