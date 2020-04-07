e631bbd5715282e36db55cbde57a836530b5ed20
A10ND <alon121211@gmail.com>
2020-04-07 15:40:10 +0300

    Created apiConfig File and exported it with index.js file pattern.

---
+++ src/config/apiConfig.js
@@ -0,0 +1,11 @@
+const SERVER_URL = "http://localhost:3001";
+const HEADERS = {
+  "Content-Type": "application/json",
+};
+
+let apiConfig = {
+  SERVER_URL,
+  HEADERS,
+};
+
+export default apiConfig;
