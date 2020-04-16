6e52ae0c063d8ed3978767e6f3a14fd25cdbf4d4
A10ND <alon121211@gmail.com>
2020-04-16 18:49:08 +0300

    Added Some base Modal stylings

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -3,24 +3,51 @@ import ReactModal from "react-modal";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import { Colors } from "../misc";
+import { Button } from "./";
+
+const ModalWrapper = styled.div({});
+
+const modalStyle = {
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    left: "15%",
+    right: "15%",
+    width: "70%",
+  },
+};
 
 const TitleWrapper = styled.h3({
-  textAlign: "center",
+  alignSelf: "center",
   color: Colors.title,
+});
+
+const ButtonWrapper = styled.div({
+  alignSelf: "flex-end",
 });
 
 let Modal = (props) => {
   return (
-    <div>
-      <ReactModal isOpen={true} contentLabel={props.title}>
+    <ModalWrapper>
+      <ReactModal
+        isOpen={true}
+        contentLabel={props.title}
+        ariaHideApp={false}
+        style={modalStyle}
+      >
+        <ButtonWrapper>
+          <Button onButtonClick={props.onCloseModal} text="X" />
+        </ButtonWrapper>
         <TitleWrapper>Add New {props.title} Item</TitleWrapper>
       </ReactModal>
-    </div>
+    </ModalWrapper>
   );
 };
 
 Modal.propTypes = {
   title: PropTypes.string,
+  onCloseModal: PropTypes.func,
 };
 
 export default Modal;
