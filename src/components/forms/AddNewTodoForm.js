23d855adb73f222aa396a784e5c1ffce812bb95b
A10ND <alon121211@gmail.com>
2020-04-18 21:09:30 +0300

    Added base AddNewTodoForm component

---
+++ src/components/forms/AddNewTodoForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Input } from "../common/Inputs";
+import styled from "styled-components";
+
+const FormWrapper = styled.div({
+  display: "flex",
+  flexDirection: "column",
+});
+
+let AddNewTodoForm = (props) => {
+  const [formDetails, setFormDetails] = useState({
+    authorName: "",
+    avatarUrl: "",
+    content: "",
+    title: "",
+  });
+
+  return (
+    <FormWrapper>
+      <Input label="Author Name: " />
+      <Input label="Avatar Url: " />
+      <Input label="Content: " />
+      <Input label="Title: " />
+    </FormWrapper>
+  );
+};
+
+export default AddNewTodoForm;
+
+AddNewTodoForm.propTypes = {};
---
+++ src/components/forms/index.js
@@ -0,0 +1,1 @@
+export { default as AddNewTodoForm } from "./AddNewTodoForm";
