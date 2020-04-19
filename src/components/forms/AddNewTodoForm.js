ef5d3e7994f484a2917f328ac3b173fc0526001e
AlonDoron <alon121211@gmail.com>
2020-04-19 17:18:47 +0300

    Updated Input props and created handleFormSubmit function

--- src/components/forms/AddNewTodoForm.js
+++ src/components/forms/AddNewTodoForm.js
@@ -1,31 +1,71 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Input } from "../common/Inputs";
 import styled from "styled-components";
+import { useForm } from "../utils";
+import { Button } from "../common";
 
-const FormWrapper = styled.div`
+const FormWrapper = styled.form`
   display: flex;
   flex-direction: column;
 `;
 
 let AddNewTodoForm = (props) => {
-  const [formDetails, setFormDetails] = useState({
-    authorName: "",
-    avatarUrl: "",
-    content: "",
-    title: "",
-  });
+  const handleFormSubmit = () => {
+    let formData = [];
+    formData.push({ name: props.name, values });
+    props.submitForm(...formData);
+  };
+
+  const [values, handleChange, handleSubmit] = useForm(handleFormSubmit);
 
   return (
-    <FormWrapper>
-      <Input label="Author Name: " />
-      <Input label="Avatar Url: " />
-      <Input label="Content: " />
-      <Input label="Title: " />
+    <FormWrapper onSubmit={handleSubmit}>
+      <Input
+        value={values.authorName || ""}
+        onChange={handleChange}
+        name="authorName"
+        label="Assigned To"
+        type="text"
+        required
+      />
+
+      <Input
+        value={values.avatarUrl || ""}
+        onChange={handleChange}
+        name="avatarUrl"
+        label="Avatar Url"
+        type="text"
+      />
+
+      <Input
+        value={values.content || ""}
+        onChange={handleChange}
+        name="content"
+        label="Content"
+        type="text"
+      />
+
+      <Input
+        value={values.title || ""}
+        onChange={handleChange}
+        name="title"
+        label="Title"
+        type="text"
+        required
+      />
+
+      <Button text="Create" type="submit" />
+      <Button text="Reset" type="reset" />
+      <Button text="Close" onButtonClick={props.closeModal} />
     </FormWrapper>
   );
 };
 
 export default AddNewTodoForm;
 
-AddNewTodoForm.propTypes = {};
+AddNewTodoForm.propTypes = {
+  name: PropTypes.string,
+  submitForm: PropTypes.func,
+  closeModal: PropTypes.func,
+};
